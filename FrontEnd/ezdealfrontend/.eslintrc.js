b91d7f0b3a4fab979c2fadf3ceeecbc7d78a2f92
AndrijaLazic <lazic.andrija223@gmail.com>
2024-01-27 11:20:11 +0100

    feat: :sparkles: Implemented eslint and prettier

---
+++ FrontEnd/ezdealfrontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports= 
+  {
+	   root:true,
+      parser:"@typescript-eslint/parser",
+      plugins:[
+        "@typescript-eslint"
+      ],
+      "env": {
+        "browser": true,
+        "amd": true,
+        "node": true
+      },
+      extends:[
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+      ],
+      parserOptions:{
+        ecmaVersion:2020,
+        "sourceType":  "module"
+      },
+      rules: {
+          semi: [2,"always"],
+          "space-before-function-paren": ["error", "never"],
+          "@typescript-eslint/no-non-null-assertion": "off",//Disallow non-null assertions using the ! postfix operator.
+          "@typescript-eslint/no-namespace": "on",
+          "@typescript-eslint/explicit-module-boundary-types": "off",
+			 "@typescript-eslint/no-explicit-any": "off",
+          '@typescript-eslint/no-var-requires': 0,
+      }
+  };
